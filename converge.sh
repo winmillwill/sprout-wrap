0d51a3ae347741b8467b70bbb31b4169639cd284
William Milton <will@d60-65-151-213.wowway.com>
2015-04-18 16:58:06 -0400

    misc fixes for bootstrapping

--- converge.sh
+++ converge.sh
@@ -4,4 +4,5 @@ set -e
 
 pushd $(dirname $0)
 [[ ! `which chef` ]] && ./getchef.sh
+[[ ! `chef gem list --local | grep soloist` ]] && chef gem install soloist
 chef exec soloist
--- getchef.sh
+++ getchef.sh
@@ -4,6 +4,7 @@ version="0.4.0-1"
 file="chefdk-${version}.dmg"
 if [[ ! -f $file ]]
 then
-  wget "https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/${file}"
+  curl "https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/${file}" -o ${file}
 fi
-open $file
+open -W -n $file
+open -W -n /Volumes/Chef\ Development\ Kit/*.pkg
